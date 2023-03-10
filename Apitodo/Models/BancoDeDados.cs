a81fd15129dbc639256a1258f41303b969c3a416
DavidMacdillen <104839637+DavidMacdillen@users.noreply.github.com>
2023-03-10 12:11:53 -0300

    Add files via upload

---
+++ Apitodo/Models/BancoDeDados.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Apitodo.Models
+{
+    public class BancoDeDados:DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\Local;Initial Catalog=Apitodo;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False
+");
+        }
+    }
+}
---
+++ Apitodo/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace Apitodo.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Tarefa { get; set; }
+
+        public string Descrição { get; set; }
+    }
+}
